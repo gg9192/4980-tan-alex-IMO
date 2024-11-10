944d43e7e32f1e38025cde95d088cf356e0ba4a9
Alex Guo <aguo2@uiowa.edu>
2024-11-09 19:04:13 -0600

    added herons

--- IMO1961_p2.thy
+++ IMO1961_p2.thy
@@ -1,4 +1,45 @@
 theory IMO1961_p2
   imports "HOL-Analysis.Simplex_Content"
-begin
+begin         
+section "proof"
+
+lemma IMO1961p2:
+  fixes A B C :: "real ^ 2"
+  assumes "a = dist B C"
+  assumes "b = dist A C" 
+  assumes "c = dist A B"
+  assumes "S = content (convex hull {A, B, C})"
+  assumes "s = (a + b + c) / 2"
+  shows "(a^2) + (b^2) + (c^2) \<ge> 4 * s * (sqrt 3)"
+  using assms
+proof -
+  
+  have "S = sqrt (((a + b + c)/2)*((-a + b + c)/2)*((a - b + c)/2)*((a + b - c)/2))"
+  proof - 
+    have heron1: "S = sqrt (s * (s - a) * (s - b) * (s - c))" 
+      using heron assms(1-5) by blast
+    show ?thesis using assms(5)
+    proof -
+      have f1: "s - c \<le> (a + b - c) / 2"
+        by (simp add: assms(5))
+      have f2: "(a + b - c) / 2 \<le> s - c"
+        by (simp add: assms(5))
+      then have f3: "(a + b - c) / 2 = s - c"
+        using f1 by simp
+      have f4: "(a - b + c) / 2 = s - b"
+        using f2 f1 by simp
+      have "(- a + b + c) / 2 = s - a"
+        using f2 f1 by simp
+      then show ?thesis
+        using f4 f3 assms(5) heron1 by presburger
+    qed
+  qed 
+  
+    (*     have a4: "((a + - b + c) / 2) = ((a - b + c) / 2)"
+      by force *)
+  show ?thesis sorry
+qed
+
+section "equality"
+
 end
