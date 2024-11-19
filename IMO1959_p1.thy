6b3af23a1ff099fbdb4e4c234669c674b5b3b0bb
Alex Guo <aguo2@uiowa.edu>
2024-11-18 20:31:48 -0600

    fixed proof

--- IMO1959_p1.thy
+++ IMO1959_p1.thy
@@ -1,5 +1,5 @@
 theory IMO1959_p1
-  imports "HOL-Computational_Algebra.Euclidean_Algorithm"
+  imports "HOL-Computational_Algebra.Euclidean_Algorithm" "HOL.Rat"
 begin         
 
 section "helper lemmas"
@@ -19,7 +19,7 @@ lemma euclidian_one_step:
 
 section "proof for the problem"
 
-lemma IMO1959_p1:
+lemma gcd_1:
   (* this formalizes the problem using gcd rather than fractions*)
   fixes n::nat
   shows "gcd (21 * n + 4) (14 * n + 3) = 1"
@@ -36,4 +36,42 @@ proof -
     using onestep by presburger
 qed
 
+lemma cant_simp:
+  (* shows that if the gdc of the numerator and denominator are coprime, then the fraction can't be further simplified *)
+  fixes a b::nat
+  fixes f:: rat
+  assumes "gcd a b = 1"
+  assumes "f = (rat_of_nat a)/(rat_of_nat b)"
+  shows "\<not>(\<exists> c d::nat. c < a \<and> d < b \<and> ((rat_of_nat c)/rat_of_nat d) = f)"
+  using assms
+proof - 
+  have "\<exists> c d::nat. c < a \<and> d < b \<and> ((rat_of_nat c)/(rat_of_nat d)) = f \<Longrightarrow> False"
+  proof - 
+    assume "\<exists> c d::nat. c < a \<and> d < b \<and> ((rat_of_nat c)/(rat_of_nat d)) = f" 
+    then obtain c d where cd: "c < a \<and> d < b \<and> ((rat_of_nat c)/(rat_of_nat d)) = f"
+      by blast
+    then have a: "a * d = c * b"
+      by (metis (no_types, lifting) assms(2) divide_eq_0_iff frac_eq_eq less_imp_of_nat_less of_nat_eq_iff of_nat_less_0_iff of_nat_mult)
+    have "coprime a b"
+      using assms(1) by blast
+    then have "a dvd c \<and> b dvd d" using a
+      by (metis coprime_commute coprime_dvd_mult_right_iff dvd_triv_right mult.commute)
+    then show "False"
+      using cd assms(2) by fastforce
+  qed
+  then show ?thesis by auto 
+qed
+
+section "proof for the problem"
+
+lemma IMO_1959_P1:
+  fixes n::nat
+  fixes f::rat
+  assumes "f = (rat_of_nat (21 * n + 4)) / (rat_of_nat (14 * n + 3))"
+  shows "\<not>(\<exists> c d::nat. c < (21 * n + 4) \<and> d < (14 * n + 3) \<and> ((rat_of_nat c)/(rat_of_nat d)) = f)"
+  using assms
+proof -
+  show ?thesis using gcd_1 cant_simp
+    using assms by presburger
+qed
 end
